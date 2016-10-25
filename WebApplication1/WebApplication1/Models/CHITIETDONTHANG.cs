6857e6cc3e674c3efa9b9cbb31fe45dc7cfb7e6a
Nhanpc <Nhanpc@NHAN>
2016-10-25 16:46:21 +0700

    sssssssss

---
+++ WebApplication1/WebApplication1/Models/CHITIETDONTHANG.cs
@@ -0,0 +1,26 @@
+namespace WebApplication1.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("CHITIETDONTHANG")]
+    public partial class CHITIETDONTHANG
+    {
+        [Key]
+        [Column(Order = 0)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int MaDonHang { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Masach { get; set; }
+
+        public int? Soluong { get; set; }
+
+        public decimal? Dongia { get; set; }
+    }
+}
